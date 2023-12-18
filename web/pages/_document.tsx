7aba62f99e16b4501cadb9137635581a35242dc9
Matthias Mair <code@mjmair.com>
2023-12-18 02:17:50 +0100

    cleanup

--- web/pages/_document.tsx
+++ web/pages/_document.tsx
@@ -4,7 +4,6 @@ export default function Document() {
   const meta = {
     title: 'InvenHost',
     description: 'Extend InvenTree for your busniess use case.',
-    image: 'https://assets.vercel.com/image/upload/q_auto/front/vercel/dps.png',
   }
 
   return (
@@ -15,12 +14,10 @@ export default function Document() {
         <meta property="og:site_name" content={meta.title} />
         <meta property="og:description" content={meta.description} />
         <meta property="og:title" content={meta.title} />
-        <meta property="og:image" content={meta.image} />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="@matthiasjmair" />
         <meta name="twitter:title" content={meta.title} />
         <meta name="twitter:description" content={meta.description} />
-        <meta name="twitter:image" content={meta.image} />
       </Head>
       <body>
         <Main />
