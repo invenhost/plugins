b7cfaddaa457e4ad89ffadb048a17abfd32253d8
Matthias Mair <code@mjmair.com>
2025-07-25 02:43:12 +0200

    add quick links

--- web/app/layout.jsx
+++ web/app/layout.jsx
@@ -1,30 +1,48 @@
-import { Footer, Layout } from 'nextra-theme-blog'
-import { Head } from 'nextra/components'
-import 'nextra-theme-blog/style.css'
-import '../styles/main.css'
-
+import { Footer, Layout } from "nextra-theme-blog";
+import { Head } from "nextra/components";
+import "nextra-theme-blog/style.css";
+import "../styles/main.css";
+import { Anchor } from "nextra/components";
 export const metadata = {
-  title: 'InvenHost',
-  description: 'Extend InvenTree for your use case.',
+  title: "InvenHost",
+  description: "Extend InvenTree for your use case.",
   alternates: {
     types: {
-      'application/rss+xml': '/feed.xml',
-    },}
-}
+      "application/rss+xml": "/feed.xml",
+    },
+  },
+};
 
 export default async function RootLayout({ children }) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" dir="ltr" suppressHydrationWarning>
       <Head backgroundColor={{ dark: '#1c1b22', light: '#fefce8' }} />
       <body>
+
         <Layout>
+          <Header />
           {children}
           <Footer>
-              <time>{new Date().getFullYear()}</time> © InvenHost by <a href="https://mjmair.com">@matmair</a>.
-              <a href="/feed.xml" style={{float: 'right'}}>RSS</a>
+            <time>{new Date().getFullYear()}</time> © InvenHost by <a href="https://mjmair.com">@matmair</a>.
+            <a href="/feed.xml" style={{ float: 'right' }}>RSS</a>
           </Footer>
         </Layout>
       </body>
     </html>
   )
 }
+
+const Header = () => {
+  return <div style={{ flexDirection: 'row', display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+    <h1>{metadata.title}</h1>
+    <span style={{ marginBottom: '2rem' }}
+    >
+      <Anchor href={'/'} style={{ textDecoration: 'none', paddingRight: '1rem' }}>
+        About
+      </Anchor>
+      <Anchor href={'/posts'} style={{ textDecoration: 'none' }}>
+        Blog
+      </Anchor>
+    </span>
+  </div>
+};
